0aa1efea46bdc710174721f16de3cb15918cd7ed
Valentin Cocaud <v.cocaud@gmail.com>
2020-04-18 15:55:09 +0200

    :card_file_box: fix migrations

--- hasura/migrations/1587207562566_run_sql_migration/up.sql
+++
@@ -1,4 +0,0 @@
-CREATE VIEW shopping_list_ingredients AS
-SELECT shopping_list_id, ingredient_id, SUM(qte)
-FROM shopping_list_recipe_ingredients
-GROUP BY shopping_list_id, ingredient_id;
--- hasura/migrations/1587208021526_run_sql_migration/up.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW "public"."shopping_list_ingredients" AS 
- SELECT shopping_list_recipe_ingredients.shopping_list_id,
-    shopping_list_recipe_ingredients.ingredient_id,
-    sum(shopping_list_recipe_ingredients.qte) AS qte,
-    unit
-   FROM shopping_list_recipe_ingredients
-  GROUP BY shopping_list_recipe_ingredients.shopping_list_id, shopping_list_recipe_ingredients.ingredient_id, unit;
