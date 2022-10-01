48a1bf1e960f7391d7d3ec549aeee8c39dfd6274
Valentin Cocaud <v.cocaud@gmail.com>
2022-10-01 22:55:28 +0200

    :bug: temporary fix for perf issue in ingredients list screen

--- app/src/components/icons/saison/index.jsx
+++ app/src/components/icons/saison/index.jsx
@@ -9,20 +9,22 @@ import { ReactComponent as WinterEnableIcon } from "./winter_enable.svg"
 
 import "./index.css"
 
-function saison(spring, summer, fall, winter) {
-  const spring_icon = spring ? <SpringEnableIcon /> : <SpringDisableIcon />
-  const summer_icon = summer ? <SummerEnableIcon /> : <SummerDisableIcon />
-  const fall_icon = fall ? <FallEnableIcon /> : <FallDisableIcon />
-  const winter_icon = winter ? <WinterEnableIcon /> : <WinterDisableIcon />
-
-  return (
-    <div className="saison-icon text-xs">
-      <div className="spring-icon">{spring_icon}</div>
-      <div className="summer-icon">{summer_icon}</div>
-      <div className="fall-icon">{fall_icon}</div>
-      <div className="winter-icon">{winter_icon}</div>
-    </div>
-  )
+function Saison({ spring, summer, fall, winter }) {
+  // TODO: Fix perf issue on season icon
+  // const spring_icon = spring ? <SpringEnableIcon /> : <SpringDisableIcon />
+  // const summer_icon = summer ? <SummerEnableIcon /> : <SummerDisableIcon />
+  // const fall_icon = fall ? <FallEnableIcon /> : <FallDisableIcon />
+  // const winter_icon = winter ? <WinterEnableIcon /> : <WinterDisableIcon />
+  //
+  // return (
+  //   <div className="saison-icon text-xs">
+  //     <div className="spring-icon">{spring_icon}</div>
+  //     <div className="summer-icon">{summer_icon}</div>
+  //     <div className="fall-icon">{fall_icon}</div>
+  //     <div className="winter-icon">{winter_icon}</div>
+  //   </div>
+  // )
+  return null
 }
 
-export { saison }
+export { Saison }
