cb2da2b000781669724b0bf542f08f9f3491c398
Valentin Cocaud <v.cocaud@gmail.com>
2022-10-01 22:53:00 +0200

    :bug: fix missing data for units and diets in database migrations

--- hasura/migrations/1658061734006_create_table_public_diet/up.sql
+++ hasura/migrations/1658061734006_create_table_public_diet/up.sql
@@ -16,3 +16,8 @@ FOR EACH ROW
 EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
 COMMENT ON TRIGGER "set_public_diet_updated_at" ON "public"."diet" 
 IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+
+INSERT INTO diet(id, name, created_at, updated_at) VALUES
+    ('117410d4-5946-4ef8-826a-dbf13b5d2d8d', 'vegan', '2022-07-17T12:43:45.377114+00:00', '2022-07-17T14:01:21.31616+00:00'),
+    ('8b36ae7d-74f4-42b6-bd36-397ed9930f6b', 'vegetarian', '2022-07-17T12:43:53.550341+00:00', '2022-07-17T14:03:08.522193+00:00'),
+    ('7c7d0bef-edf0-4b2c-8218-62eb199755f6', 'carnivorous', '2022-07-17T12:44:12.388452+00:00','2022-07-17T14:03:28.634959+00:00');
--- hasura/migrations/1659742003205_set_fk_public_recipe_ingredient_unit_id/up.sql
+++ hasura/migrations/1659742003205_set_fk_public_recipe_ingredient_unit_id/up.sql
@@ -1,3 +1,10 @@
+UPDATE recipe_ingredient
+SET unit_id = (
+    SELECT unit.id
+    FROM unit
+    WHERE recipe_ingredient.unit = unit.short
+);
+
 alter table "public"."recipe_ingredient"
            add constraint "recipe_ingredient_unit_id_fkey"
            foreign key ("unit_id")
